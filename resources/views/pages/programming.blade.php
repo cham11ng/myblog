7aa3d5282e0d5983114c8df20420ec98e864b2d5
Sagar Chamling <sgr.raee@gmail.com>
2016-12-28 01:30:30 +0545

    programming.blade.php file created and array passed from route was displayed

---
+++ resources/views/pages/programming.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 96px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        @if (empty($languages))
+            <div class="title">No Any Languages Preferred</div>
+        @else
+            <div class="title">Something Else Here</div>
+        @endif
+
+        @foreach ($languages as $language):
+            <li>{{ $language }}</li>
+        @endforeach
+    </div>
+</div>
+</body>
+</html>
