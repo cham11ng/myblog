388043a56f007cc0db2856c6f358a2fca4872818
Sagar Chamling <sgr.raee@gmail.com>
2017-01-09 23:48:49 +0545

    title added and about page modified.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,14 +1,20 @@
-@extends('layout')
-
+@extends('layouts.front')
+@section('title', ' | about')
 @section('content')
-    @if (empty($languages))
-        <header class="title">No Any Languages Preferred</header>
-    @else
-        <header class="title">About Some Languages</header>
-    @endif
-    <ul>
-        @foreach ($languages as $language)
-        <li>{{ $language }}</li>
-        @endforeach
-    </ul>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis aspernatur quas quibusdam veniam sunt animi, est quos optio explicabo deleniti inventore unde minus, tempore enim ratione praesentium, cumque, dolores nesciunt?</p>
+            @if (empty($languages))
+                <header class="title">No Any Languages Preferred</header>
+            @else
+                <header class="title">About Some Languages</header>
+            @endif
+            <ul>
+                @foreach ($languages as $language)
+                    <li>{{ $language }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 @stop
