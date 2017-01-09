351460bdc362c2d384dc591d93e301cf2fb57859
Sagar Chamling <sgr.raee@gmail.com>
2017-01-10 02:43:10 +0545

    All Cards Designed Changed

--- resources/views/cards/cards.blade.php
+++ resources/views/cards/cards.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.front')
 @section('title', ' | cards')
 @section('content')
-    <header class="title">All Cards Here</header>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">All Cards</div>
+                <div class="panel-body">
+                    <ul>
+                        @foreach ($cards as $card)
+                            <li><a href="{{ url('/card/'.$card->card_slug) }}">{{ $card->card_title }}</a></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
