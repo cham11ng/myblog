76b41f2f578ccf3c2430804c3cc1e9d30d1c81a9
Sagar Chamling <sgr.raee@gmail.com>
2017-01-11 13:29:04 +0545

    explicit approach of adding note

--- app/Card.php
+++ app/Card.php
@@ -9,4 +9,8 @@ class Card extends Model
     public function notes() {
         return $this->hasMany(Note::class, 'card_id', 'card_id');
     }
+
+    public function addNote(Note $note) {
+        return $this->notes()->save($note);
+    }
 }
