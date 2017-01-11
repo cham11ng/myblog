02973b1b2e4cb850e15a4db3589c8a0960b7274d
Sagar Chamling <sgr.raee@gmail.com>
2017-01-11 22:50:35 +0545

    store new cards function added

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Card;
+use Illuminate\Http\Request;
 
 class CardsController extends Controller
 {
@@ -14,6 +15,15 @@ class CardsController extends Controller
     public function showCard($card_slug) {
         $card = Card::where('card_slug', '=', $card_slug)->first();
         //return $card; // json
-        return view('cards.notes', compact('card'));
+        return view('notes.notes', compact('card'));
+    }
+
+    public function storeCard(Request $request) {
+        Card::create([
+            'card_title'    => $request->card_title,
+            'card_slug'     => str_slug($request->card_title, '-')
+        ]);
+
+        return back();
     }
 }
