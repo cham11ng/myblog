2ab26f46abe43564c9049bdeb8c66aa9cd68ffa1
Sagar Chamling <sgr.raee@gmail.com>
2017-01-12 11:45:34 +0545

    Delete and Update Added, Comment Added.

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -8,14 +8,63 @@ use App\Card;
 
 class NotesController extends Controller
 {
+    /**
+     * @param Request $request
+     * @param $card_slug
+     * @return \Illuminate\Http\RedirectResponse
+     * Adding Notes to Card identified by $card_slug
+     */
     public function storeNote(Request $request, $card_slug) {
         $card = Card::where('card_slug', '=', $card_slug)->first();
 
-        $card->notes()->create([
+        /**
+         * Implicit Method
+         */
+        /*$card->notes()->create([
             'note_content'  => $request->note_content
-        ]);
+        ]);*/
 
-        //$card->addNote(new Note($request->all()));
+        /**
+         * Explicit Method
+         */
+        $card->addNote(new Note([
+            'note_content' => $request->note_content
+        ]));
+
+        return back();
+    }
+
+    /**
+     * @param $note_id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * EditNote identified by$note_id View Page
+     */
+    public function editNote($note_id) {
+        $note = Note::where('note_id', '=', $note_id)->with('card')->first();
+
+        return view('notes.edit_note', compact('note'));
+    }
+
+    /**
+     * @param Request $request
+     * @param $note_id
+     * @return \Illuminate\Http\RedirectResponse
+     * Notes Update Patch
+     */
+    public function updateNote(Request $request, $note_id) {
+        Note::where('note_id', '=', $note_id)
+            ->update(['note_content' => $request->note_content]);
+
+        return back();
+    }
+
+    /**
+     * @param $note_id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function deleteNote($note_id) {
+        Note::where('note_id', '', $note_id)
+            ->delete();
 
         return back();
     }
