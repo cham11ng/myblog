d0f7e0eefbf81180476e432f0f7bcabbb5591d66
Sagar Chamling <sgr.raee@gmail.com>
2017-01-12 23:01:42 +0545

    relation with user

--- app/Note.php
+++ app/Note.php
@@ -13,12 +13,16 @@ class Note extends Model
     /**
      * @var array
      */
-    protected $fillable = ['note_content'];
+    protected $fillable = ['note_content', 'user_id'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function card() {
-        return $this->belongsTo(Card::class, 'card_id', 'card_id');
+        return $this->belongsTo(Card::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
