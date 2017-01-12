42b779303a3a5cf208819b5c37b0ff4c0f57fb48
Sagar Chamling <sgr.raee@gmail.com>
2017-01-12 12:03:02 +0545

    All manipulation routes added and documented. Alt+Ins >> PHPDocs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,23 +11,39 @@
 |
 */
 
+/**
+ * Pages Controller
+ */
 Route::get('/', 'PagesController@getHome');
-
 Route::get('/about', 'PagesController@getAbout');
-
 Route::get('/contact', 'PagesController@getContact');
 
-Route::get('/empty', function () {
-    $languages = [];
-    return view('pages.about', compact('languages'));
-});
+/**
+ * CardsController
+ */
+Route::get('/cards', 'CardsController@getCards');
+Route::get('/card/{card_slug}/notes', 'CardsController@showCard');
 
-Route::get('cards', 'CardsController@getCards');
+Route::post('/card/add_card', 'CardsController@storeCard');
 
-Route::get('card/{card_slug}', 'CardsController@showCard');
+Route::get('/card/{card_slug}/edit', 'CardsController@editCard');
+Route::patch('/card/{card_slug}', 'CardsController@updateCard');
 
-Route::post('card/{card_slug}/add_note', 'NotesController@storeNote');
+Route::delete('/card/{card_slug}/delete', 'CardsController@deleteCard');
 
+Route::post('/card/{card_slug}/add_note', 'NotesController@storeNote');
+
+/**
+ * NotesController
+ */
+Route::get('/note/{note_id}/edit', 'NotesController@editNote');
+Route::patch('/note/{note_id}', 'NotesController@updateNote');
+
+Route::delete('/note/{note_id}/delete', 'NotesController@deleteNote');
+
+/**
+ * Error Page
+ */
 Route::get('error', function () {
     return view('errors.503');
 });
