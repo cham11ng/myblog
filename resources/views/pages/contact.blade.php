f6639332442c5298584d18f0f96e5e546007c491
Sagar Chamling <sgr.raee@gmail.com>
2017-01-12 11:47:29 +0545

    Contact Page with panel layout

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,63 +1,57 @@
-@extends('layouts.front')
+@extends('layouts.panel')
 @section('title', ' | contact')
-@section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Contact Me</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST">
-                        {{ csrf_field() }}
+@section('panel-heading')
+    Contact Me
+@endsection
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address:</label>
+@section('panel-body')
+    <form class="form-horizontal" role="form" method="POST">
+        {{ csrf_field() }}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <label for="email" class="col-md-4 control-label">E-Mail Address:</label>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('subject') ? ' has-error' : '' }}">
-                            <label for="subject" class="col-md-4 control-label">Subject:</label>
-                            <div class="col-md-6">
-                                <input id="subject" class="form-control" name="subject" value="{{ old('subject') }}">
-
-                                @if ($errors->has('subject'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('subject') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
-                            <label for="message" class="col-md-4 control-label">Message:</label>
-                            <div class="col-md-6">
-                                <textarea name="message" id="message" class="form-control" placeholder="Type your message here...">{{ old('message') }}</textarea>
-                                @if ($errors->has('message'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('message') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Submit
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="col-md-6">
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
+                @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
             </div>
         </div>
-    </div>
+
+        <div class="form-group{{ $errors->has('subject') ? ' has-error' : '' }}">
+            <label for="subject" class="col-md-4 control-label">Subject:</label>
+            <div class="col-md-6">
+                <input id="subject" class="form-control" name="subject" value="{{ old('subject') }}">
+
+                @if ($errors->has('subject'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('subject') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
+            <label for="message" class="col-md-4 control-label">Message:</label>
+            <div class="col-md-6">
+                <textarea name="message" id="message" class="form-control" placeholder="Type your message here...">{{ old('message') }}</textarea>
+                @if ($errors->has('message'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('message') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">
+                    <i class="fa fa-btn fa-sign-in"></i> Submit
+                </button>
+            </div>
+        </div>
+    </form>
 @endsection
