ded9d9b653a1a2b19b067c1d90ee717ce308d284
Sagar Chamling <sgr.raee@gmail.com>
2017-01-18 00:00:25 +0545

    Soft Delete Added, it doesn't delete model instead keep timestamps when it was deleted.

--- app/Card.php
+++ app/Card.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Card
@@ -10,6 +11,15 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Card extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
     /**
      * @var array
      */
