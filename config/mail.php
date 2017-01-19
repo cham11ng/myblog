86afe11ba54ce867d73cd24366524f957794a913
Sagar Chamling <sgr.raee@gmail.com>
2017-01-19 11:22:45 +0545

    Mail File Subject and From field Changed

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'support@cham11ng.blog', 'name' => 'cham11ng Blog Support'],
 
     /*
     |--------------------------------------------------------------------------
